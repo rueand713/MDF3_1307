1e89fdb4417d51ddd8093ecd4fcf3358d5aef51a
Rueben Anderson <rueand713@fullsail.edu>
2013-07-31 02:17:04 -0500

    Added the start screen activity

--- DocuWeb/src/com/randerson/docuweb/MainActivity.java
+++ DocuWeb/src/com/randerson/docuweb/MainActivity.java
@@ -1,114 +1,69 @@
 package com.randerson.docuweb;
 
-import java.io.File;
-
-import libs.FileSystem;
-
-import com.randerson.JS.JSInterface;
-import com.randerson.interfaces.ActivityInterface;
-
-import android.os.Bundle;
-import android.annotation.SuppressLint;
+import libs.InterfaceManager;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.util.Log;
+import android.os.Bundle;
 import android.view.Menu;
-import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
+import android.view.View;
+import android.widget.Button;
 
-@SuppressLint("SetJavaScriptEnabled")
-public class MainActivity extends Activity implements ActivityInterface {
+public class MainActivity extends Activity {
 
-	final String URL = "https://dl.dropboxusercontent.com/u/68223018/apps/docuweb/docuweb.html";
-	
-	JSInterface interfaceJS;
-	WebView webApp;
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	
-		interfaceJS = new JSInterface(this);
+		setContentView(R.layout.activity_title);
 		
-		// create the webview from the layout
-		webApp = (WebView) findViewById(R.id.webView);
+		final InterfaceManager UIFactory = new InterfaceManager(this);
 		
-		// verify that the webview is valid
-		if (webApp != null)
+		// create the button from the layout
+		Button newTagButton = (Button) findViewById(R.id.new_note);
+		
+		// verify that the button is valid
+		if (newTagButton != null)
 		{
-			WebChromeClient chromeClient = new WebChromeClient();
-			
-			// set the webview to allow the use of javascript
-			WebSettings settings = webApp.getSettings();
-			settings.setJavaScriptEnabled(true);
-			
-			webApp.setWebChromeClient(chromeClient);
-			
-			// setup the webview js interface object and namespace hook
-			webApp.addJavascriptInterface(interfaceJS, "Native");
-			
-			// load the hybrid app webpage url into the webview
-			webApp.loadUrl(URL);
-			
+			// set the click listener for the button
+			newTagButton.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					// create the intent to start the note activity
+					Intent noteActivity = UIFactory.makeIntent(NoteActivity.class);
+					
+					// verify the intent is valid
+					if (noteActivity != null)
+					{
+						// start the intent
+						startActivity(noteActivity);
+					}
+				}
+			});
 		}
 		
+		// set the click listener for the button
+		Button viewTagButton = (Button)findViewById(R.id.view_note);
+		
+		// verify that the button is valid
+		if (viewTagButton != null)
+		{
+			// set the click listener for the button
+			viewTagButton.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					
+					
+				}
+			});
+		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public void launchCamera() {
-		
-		// intent for launching the camera app
-		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-		
-		// verify that the intent is valid
-		if (camera != null)
-		{
-			startActivityForResult(camera, 0);
-		}
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		
-		if (requestCode == 0)
-		{
-			if (data != null)
-			{
-				Bundle bundle = data.getExtras();
-				
-				if (bundle != null && bundle.containsKey("data"))
-				{
-					// create the bitmap image and save it to the application file system
-					Bitmap image = (Bitmap) bundle.get("data");
-					
-					// capture the system write data returned
-					Object[] result = FileSystem.writeBitmapFile(this, image, "default_image.jpg", false, false, 90, Bitmap.CompressFormat.JPEG);
-					
-					// grab the file path from the write data
-					String path = (String) result[1];
-					
-					// pass the image path to the js interface
-					interfaceJS.PATH = path;
-					
-					Log.i("Path", path);
-					
-					// call method to retrieve the image for the webview
-					interfaceJS.callJSRetrieveImage(webApp);
-				}
-			}
-		}
+		// TODO Auto-generated method stub
+		return super.onCreateOptionsMenu(menu);
 	}
 
 }
---
+++ DocuWeb/src/com/randerson/docuweb/NoteActivity.java
@@ -0,0 +1,111 @@
+package com.randerson.docuweb;
+
+import libs.FileSystem;
+
+import com.randerson.JS.JSInterface;
+import com.randerson.interfaces.ActivityInterface;
+
+import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.util.Log;
+import android.view.Menu;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class NoteActivity extends Activity implements ActivityInterface {
+
+	final String URL = "https://dl.dropboxusercontent.com/u/68223018/apps/docuweb/docuweb.html";
+	
+	JSInterface interfaceJS;
+	WebView webApp;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	
+		interfaceJS = new JSInterface(this);
+		
+		// create the webview from the layout
+		webApp = (WebView) findViewById(R.id.webView);
+		
+		// verify that the webview is valid
+		if (webApp != null)
+		{
+			WebChromeClient chromeClient = new WebChromeClient();
+			
+			// set the webview to allow the use of javascript
+			WebSettings settings = webApp.getSettings();
+			settings.setJavaScriptEnabled(true);
+			
+			webApp.setWebChromeClient(chromeClient);
+			
+			// setup the webview js interface object and namespace hook
+			webApp.addJavascriptInterface(interfaceJS, "Native");
+			
+			// load the hybrid app webpage url into the webview
+			webApp.loadUrl(URL);
+			
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public void launchCamera() {
+		
+		// intent for launching the camera app
+		Intent camera = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+		
+		// verify that the intent is valid
+		if (camera != null)
+		{
+			startActivityForResult(camera, 0);
+		}
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		
+		if (requestCode == 0)
+		{
+			if (data != null)
+			{
+				Bundle bundle = data.getExtras();
+				
+				if (bundle != null && bundle.containsKey("data"))
+				{
+					// create the bitmap image and save it to the application file system
+					Bitmap image = (Bitmap) bundle.get("data");
+					
+					// save the image and capture the system write data returned
+					Object[] result = FileSystem.writeBitmapFile(this, image, "default_image.jpg", false, false, 90, Bitmap.CompressFormat.JPEG);
+					
+					// grab the file path from the write data
+					String path = (String) result[1];
+					
+					// pass the image path to the js interface
+					interfaceJS.PATH = path;
+					
+					Log.i("Path", path);
+					
+					// call method to retrieve the image for the webview
+					interfaceJS.callJSRetrieveImage(webApp);
+				}
+			}
+		}
+	}
+
+}
