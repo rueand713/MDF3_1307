3569524d9ea5b788296531fa5e32cfaacf7d703e
Rueben Anderson <rueand713@fullsail.edu>
2013-07-31 14:23:38 -0500

    Update
    Added the list activity functionality

--- DocuWeb/gen/com/randerson/docuweb/R.java
+++ DocuWeb/gen/com/randerson/docuweb/R.java
@@ -29,16 +29,20 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int directions=0x7f080002;
-        public static final int new_note=0x7f080003;
-        public static final int title=0x7f080001;
-        public static final int view_note=0x7f080004;
-        public static final int webView=0x7f080000;
+        public static final int action_settings=0x7f080007;
+        public static final int data_list=0x7f080000;
+        public static final int directions=0x7f080003;
+        public static final int new_note=0x7f080004;
+        public static final int note_section=0x7f080006;
+        public static final int title=0x7f080002;
+        public static final int view_note=0x7f080005;
+        public static final int webView=0x7f080001;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int activity_title=0x7f030001;
+        public static final int activity_list=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int activity_title=0x7f030002;
+        public static final int list_data=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -46,6 +50,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int blank=0x7f050007;
         public static final int copy=0x7f050006;
         public static final int hello_world=0x7f050002;
         public static final int instructions=0x7f050005;
--- DocuWeb/src/com/randerson/docuweb/ListActivity.java
+++ DocuWeb/src/com/randerson/docuweb/ListActivity.java
@@ -1,7 +1,70 @@
 package com.randerson.docuweb;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import libs.FileSystem;
+import libs.InterfaceManager;
+import libs.UniArray;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
 
 public class ListActivity extends Activity {
 
+	InterfaceManager UIFactory = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_list);
+		
+		// create the uifactory singleton
+		UIFactory = new InterfaceManager(this);
+		
+		// read the saved data
+		UniArray memory = (UniArray) FileSystem.readObjectFile(this, "save_data", true);
+		
+		if (memory != null)
+		{
+			// create the array list data object
+			ArrayList<HashMap<String, String>> data = new ArrayList<HashMap<String, String>>();
+			
+			// array of string keys
+			String[] keys = memory.getAllObjectKeys();
+			
+			// iterate over the memory uniarray and retrieve the keys for each index
+			for (int i = 0; i < memory.objectsLength(); i++)
+			{	
+				// the object key
+				String key = keys[i];
+				
+				// create the hashmap medium for the array list
+				HashMap<String, String> tempHash = new HashMap<String, String>();
+				
+				// add the key to the hash of memory object titles (keys)
+				tempHash.put("note " + (i+1), key);
+				
+				// add the hashmap to the array list
+				data.add(tempHash);
+			}
+			
+			// create the listview from the layout
+			ListView lsv = (ListView) findViewById(R.id.data_list);
+			
+			// setup the listview
+			UIFactory.createList(lsv, data, R.layout.list_data, new String[] {"Note"}, new int[]{R.id.note_section});
+		}
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	
+	
 }
